cafadb560dbaeea0ed93b1c8284a1d15af9d1df8
zyy <897080264@qq.com>
2016-06-17 11:13:50 +0800

    Gzip压缩二进制数据流

---
+++ src/main/java/util/GzipCompressUtil.java
@@ -0,0 +1,104 @@
+package util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * Created by zyy on 2016/6/17.
+ */
+public class GzipCompressUtil {
+
+    /**
+     * Gzip压缩
+     */
+    public static byte[] gzip(byte[] sourceBytes) {
+        if (sourceBytes == null || sourceBytes.length <= 0) {
+            return null;
+        }
+        byte[] targetBytes = null;
+        ByteArrayOutputStream bos = null;
+        GZIPOutputStream gzipOs = null;
+        try {
+            bos = new ByteArrayOutputStream();
+            gzipOs = new GZIPOutputStream(bos);
+            gzipOs.write(sourceBytes);
+            gzipOs.finish();
+            targetBytes = bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (gzipOs != null) {
+                try {
+                    gzipOs.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (bos != null) {
+                        try {
+                            bos.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return targetBytes;
+    }
+
+    /**
+     * Gzip解压缩
+     * */
+    public static byte[] gunzip(byte[] sourceBytes) {
+        if (sourceBytes == null || sourceBytes.length <= 0) {
+            return null;
+        }
+        byte[] targetBytes = null;
+        ByteArrayInputStream bio = null;
+        GZIPInputStream gzipis = null;
+        ByteArrayOutputStream bos = null;
+        try {
+            bio = new ByteArrayInputStream(sourceBytes);
+            gzipis = new GZIPInputStream(bio);
+            bos = new ByteArrayOutputStream();
+            byte[] tempBuffer = new byte[1024];
+            int numbers = -1;
+            while ((numbers = gzipis.read(tempBuffer, 0, tempBuffer.length)) != -1) {
+                bos.write(tempBuffer, 0, numbers);
+            }
+            targetBytes = bos.toByteArray();
+            bos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(bos!=null){
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(gzipis!=null){
+                        try {
+                            gzipis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            if(bio!=null){
+                                try {
+                                    bio.close();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return targetBytes;
+
+    }
+}
